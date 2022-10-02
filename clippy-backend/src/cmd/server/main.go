ff0cdadaca289e44972d07a1d11dfabba794ad92
Thibaux <thibaux.swildens@gmail.com>
2022-10-02 22:34:43 +0200

    Added env support

--- clippy-backend/src/cmd/server/main.go
+++ clippy-backend/src/cmd/server/main.go
@@ -1,8 +1,9 @@
 package main
 
 import (
-	
 	router "backend/internal/Host"
+
+
 )
 
 func main() {
--- clippy-backend/src/internal/Host/router.go
+++ clippy-backend/src/internal/Host/router.go
@@ -1,23 +1,31 @@
 package router
 
 import (
+	"os"
 	"fmt"
 	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
+    "github.com/joho/godotenv"
 
 	Handlers "backend/internal/Host/Handlers"
 )
 
 func Router() {
+	err := godotenv.Load()
+	if err != nil {
+	  log.Fatal("Error loading .env file")
+	}
+  
 	router := mux.NewRouter().StrictSlash(true)
 
 	router.HandleFunc("/", Handlers.GetHealth)
 	router.HandleFunc("/health", Handlers.GetHealth)
 	router.HandleFunc("/items", Handlers.GetItems)
 
-	var port = ":8080"
+	var port = os.Getenv("PORT")
+
 	fmt.Printf("Server starting on port: %v", port)
 	log.Fatal(http.ListenAndServe(port, router))
 }
