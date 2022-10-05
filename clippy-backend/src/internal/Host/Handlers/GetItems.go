0ad56afd53fc333bc02a83f6a31f75a4bc9953a4
Thibaux <thibaux.swildens@gmail.com>
2022-10-05 17:42:44 +0200

    Temp commit

--- clippy-backend/src/internal/Host/Handlers/GetItems.go
+++ clippy-backend/src/internal/Host/Handlers/GetItems.go
@@ -4,39 +4,36 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/nitishm/go-rejson/v4"
+
+	Redis	 "backend/internal/Infrastructure/Redis"		
 	Services "backend/internal/Services"
 )
 
-type Article struct {
-	Id      string `json:"Id"`
-	Title   string `json:"Title"`
-	Desc    string `json:"desc"`
-	Content string `json:"content"`
-}
-
-var Articles []Article
 
 func GetItems(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Endpoint Hit: getArticles")
-	Articles = []Article{
-		{Id: "1", Title: "Hello", Desc: "Article Description", Content: "Article Content"},
-		{Id: "2", Title: "Hello 2", Desc: "Article Description", Content: "Article Content"},
-	}
 
 	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
 	// jsonBytes, err := json.Marshal(Articles)
 	// if err != nil {
 	// 	fmt.Printf("Error")
 	// }
 
-	Services.GetItems()
+	rh := rejson.NewReJSONHandler()
+
+	cli := Redis.ConnectToRedis()
+	rh.SetGoRedisClient(cli)
+
+	items := Services.GetAllItems(rh)
+	fmt.Println("v*", items)
 
 	// if err != nil {
 	// 	fmt.Printf("Noo")
 	// }
 
 	// fmt.Printf(result)
-	
-	w.WriteHeader(http.StatusOK)
+
 	// w.Write(jsonBytes)
 }
--- clippy-backend/src/internal/Services/Items.go
+++ clippy-backend/src/internal/Services/Items.go
@@ -2,16 +2,47 @@ package Services
 
 import (
 	"fmt"
-	// "context"
-	// "log"
 
-	// "github.com/nitishm/go-rejson/v4"
+	"github.com/nitishm/go-rejson/v4"
 
-	// Redis "backend/internal/Infrastructure/Redis"
-
-	// goredis "github.com/go-redis/redis/v8"
+	Models	"backend/internal/Infrastructure/Models"
+	// Redis 	"backend/internal/Infrastructure/Redis"
+	// Utils 	"backend/internal/Services/Utils"
 )
 
-func GetItems() {
+type Item struct {
+	id		string
+	name	string
+}
+
+func GetAllItems(rh *rejson.Handler) Models.Items {
 	fmt.Println("hsdf")
+
+	var items Models.Items
+
+	i := Models.Item{Id: "123", Name: "thisName"}
+
+	items = append(items, i)
+
+	Ii := Models.Item
+
+	res, err := rh.JSONSet("items", ".", Ii)
+	if err != nil {
+		fmt.Printf("Failed to JSONGet")
+		return items
+	}
+	fmt.Println("obj:", res)
+
+	// res, err := rh.JSONGet("items", ".")
+	// if err != nil {
+	// 	fmt.Printf("Failed to JSONGet")
+	// 	return items
+	// }
+
+	fmt.Println("got obj:", res)
+
+	// Utils.HandleError(err)
+	
+
+	return items
 }
