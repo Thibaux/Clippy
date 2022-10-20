55a3ff6f88046bf7a3d247402549eb5d88cefbf9
Thibaux <thibaux.swildens@gmail.com>
2022-10-20 12:35:44 +0200

    Removed bootstaped files.

---
+++ clippy-frontend/src/views/ItemsOverviewView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import ItemsOverview from "../components/ItemsOverview.vue";
+</script>
+
+<template>
+  <main>
+    <ItemsOverview msg="sadfsfs" />
+  </main>
+</template>
---
+++ clippy-frontend/src/views/index.ts
@@ -0,0 +1,1 @@
+export { default as ItemsOverviewView } from "./ItemsOverviewView.vue";
