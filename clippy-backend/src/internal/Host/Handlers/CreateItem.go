42c894ef481595b67b3bd0cf57f2de41cecef1db
Thibaux Swildens <38690826+Thibaux@users.noreply.github.com>
2022-10-20 11:39:36 +0200

    Feat/item card (#3)
    * Installed packages
    
    * Installed tailwind
    
    * Created fist version of item card with dummy data
    
    * Inital uuid work
    
    * Updated frontend
    
    * Installed deps
    
    * Removed angular

--- clippy-backend/src/internal/Host/Handlers/CreateItem.go
+++ clippy-backend/src/internal/Host/Handlers/CreateItem.go
@@ -8,23 +8,27 @@ import (
 	Redis "backend/internal/Infrastructure/Redis"
 	Services "backend/internal/Services"
 	Utils "backend/internal/Services/Utils"
+	Models "backend/internal/Infrastructure/Models"
 )
 
 func CreateItem(w http.ResponseWriter, r *http.Request) {
+    var item Models.Item
+
 	fmt.Println("Endpoint Hit: CreateItem")
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 
 	rh := Redis.ConnectToRedis()
-	item := Utils.ParseRequestBody(r)
+	item = Utils.ParseRequestBody(r)
+
+    id := "1234234"
+    item = append(item, id)
 
 	updatedItems := Services.CreateOneItem(rh, item)
 	jsonBytes, err := json.Marshal(updatedItems)
 	if err != nil {
 		fmt.Printf("Error")
 	}
-	
+
 	w.Write(jsonBytes)
 }
-
-
--- clippy-backend/src/internal/Infrastructure/Models/Models.go
+++ clippy-backend/src/internal/Infrastructure/Models/Models.go
@@ -1,8 +1,11 @@
 package Models
 
 type Item struct {
-	Id   string `json:"id"`
-	Name string `json:"name"`
+	Id          string `json:"id"`
+	Title       string `json:"title"`
+	Content     string `json:"content"`
+	UpdatedAt   string `json:"updatedAt"`
+	CreatedAt   string `json:"createdAt"`
 }
 
 type Items []Item
